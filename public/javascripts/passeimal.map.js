0a2a9574ce1df7b4a58dcbcc97b127e760344b99
jplindgren <jplindgrenbr@hotmail.com>
2015-05-07 16:17:38 -0300

    add google place search POC

--- public/javascripts/passeimal.map.js
+++ public/javascripts/passeimal.map.js
@@ -28,49 +28,50 @@
 
 		var addAutoCompleteEventListener = function(context){
 			var infowindow = new google.maps.InfoWindow();
-		  var marker = new google.maps.Marker({
-		    map: context.map,
-		    anchorPoint: new google.maps.Point(0, -29)
-		  });
+			  var marker = new google.maps.Marker({
+			    map: context.map,
+			    anchorPoint: new google.maps.Point(0, -29)
+			  });
 
 			google.maps.event.addListener(context.autocomplete, 'place_changed', function() {
-		    infowindow.close();
-		    marker.setVisible(false);
-		    var place = context.autocomplete.getPlace();
-		    if (!place.geometry) {
-		      window.alert("Autocomplete's returned place contains no geometry");
-		      return;
-		    }
+				console.log(context.autocomplete);
+			    infowindow.close();
+			    marker.setVisible(false);
+			    var place = context.autocomplete.getPlace();
+			    if (!place.geometry) {
+			      window.alert("Autocomplete's returned place contains no geometry");
+			      return;
+			    }
 
-		    // If the place has a geometry, then present it on a map.
-		    if (place.geometry.viewport) {
-		      context.map.fitBounds(place.geometry.viewport);
-		    } else {
-		      context.map.setCenter(place.geometry.location);
-		      context.map.setZoom(17);  // Why 17? Because it looks good.
-		    }
-		    marker.setIcon(/** @type {google.maps.Icon} */({
-		      url: place.icon,
-		      size: new google.maps.Size(71, 71),
-		      origin: new google.maps.Point(0, 0),
-		      anchor: new google.maps.Point(17, 34),
-		      scaledSize: new google.maps.Size(35, 35)
-		    }));
-		    marker.setPosition(place.geometry.location);
-		    marker.setVisible(true);
+			    // If the place has a geometry, then present it on a map.
+			    if (place.geometry.viewport) {
+			      context.map.fitBounds(place.geometry.viewport);
+			    } else {
+			      context.map.setCenter(place.geometry.location);
+			      context.map.setZoom(17);  // Why 17? Because it looks good.
+			    }
+			    marker.setIcon(/** @type {google.maps.Icon} */({
+			      url: place.icon,
+			      size: new google.maps.Size(71, 71),
+			      origin: new google.maps.Point(0, 0),
+			      anchor: new google.maps.Point(17, 34),
+			      scaledSize: new google.maps.Size(35, 35)
+			    }));
+			    marker.setPosition(place.geometry.location);
+			    marker.setVisible(true);
 
-		    var address = '';
-		    if (place.address_components) {
-		      address = [
-		        (place.address_components[0] && place.address_components[0].short_name || ''),
-		        (place.address_components[1] && place.address_components[1].short_name || ''),
-		        (place.address_components[2] && place.address_components[2].short_name || '')
-		      ].join(' ');
-		    }
+			    var address = '';
+			    if (place.address_components) {
+			      address = [
+			        (place.address_components[0] && place.address_components[0].short_name || ''),
+			        (place.address_components[1] && place.address_components[1].short_name || ''),
+			        (place.address_components[2] && place.address_components[2].short_name || '')
+			      ].join(' ');
+			    }
 
-		    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-		    infowindow.open(context.map, marker);
-		  });
+			    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+			    infowindow.open(context.map, marker);
+		  	});
 		};
 
 		return publicApi = {
